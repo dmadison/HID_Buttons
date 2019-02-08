051003d5f8c8f6577a4fab5da3606cfaf0b436d5
David Madison <dmadison@users.noreply.github.com>
2019-02-08 10:55:58 -0500

    Add Teensy Keyboard compatibility
    Almost direct compatibility, but requires resizing the stored 'key' member to uint16_t in order to support modifier keys.

--- src/variants/HID_Button_Keyboard.h
+++ src/variants/HID_Button_Keyboard.h
@@ -29,10 +29,38 @@
 
 #include "HID_Button_API.h"
 
-#if defined(KEYBOARD_h) && defined(_USING_HID)
+ /*
+  * Include Guard Info
+  * --------------------
+  *
+  * Arduino Boards:
+  *     KEYBOARD_h   built-in library header include guard
+  *     _USING_HID   defined in HID.h if USB is enabled (USBCON)
+  *
+  * Teensy:
+  *     TEENSYDUINO       defined if using any Teensy board
+  *     USB_KEYBOARDONLY  defined by boards.txt if using USB type "Keyboard"
+  *     USB_HID           defined by boards.txt if using USB type "Keyboard + Mouse + Joystick"
+  *     USB_SERIAL_HID    defined by boards.txt if using USB type "Serial + Keyboard + Mouse + Joystick"
+  *     USB_TOUCHSCREEN      defined by boards.txt if using USB type "Keyboard + Touch Screen"
+  *     USB_HID_TOUCHSCREEN  defined by boards.txt if using USB type "Keyboard + Mouse + Touch Screen"
+  */
+
+#if (defined(KEYBOARD_h) && defined(_USING_HID)) || \
+	(defined(TEENSYDUINO) && \
+	(defined(USB_KEYBOARDONLY) || defined(USB_HID) || defined(USB_SERIAL_HID) \
+	 || defined(USB_TOUCHSCREEN) || defined(USB_HID_TOUCHSCREEN)))
+
 class KeyboardButton : public HID_Button {
 public:
-	KeyboardButton(char k) :
+
+#ifdef TEENSYDUINO
+	using KeySize = uint16_t;
+#else
+	using KeySize = uint8_t;
+#endif
+
+	KeyboardButton(KeySize k) :
 		key(k) {}
 
 	static void releaseAll() {
@@ -52,7 +80,7 @@ protected:
 		state ? Keyboard.press(key) : Keyboard.release(key);
 	}
 
-	const char key;
+	const KeySize key;
 };
 #endif
 
